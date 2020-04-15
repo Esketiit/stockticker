56bac6a8a74040a2ff1eeafdbcd6576ff16c6359
github username <github email address>
2020-04-15 11:05:45 -0400

    dice rolls working

--- stockticker_frontend/index.js
+++ stockticker_frontend/index.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", (event) => {
-    let grid = document.getElementById("board_grid")
+    let grid = document.querySelector(".grid-container")
     let die = document.querySelector(".die")
     let ani_c = document.getElementById("animal_crossing")
     let toilet = document.getElementById("toilet_paper")
@@ -8,19 +8,6 @@ document.addEventListener("DOMContentLoaded", (event) => {
 
     fetchGame()
 
-    function buildGrid() {
-        let counter = 0
-        let grid = document.querySelector('.grid-container')
-        while (counter < 40) {
-            let div = document.createElement('div')
-            div.setAttribute("class", "grid-item")
-            grid.appendChild(div)
-            console.log(counter)
-            counter++
-        }
-    }
-
-    //uses fetch to get data for a game from database
     function fetchGame() {
         fetch("http://localhost:3000/api/games")
             .then(response => response.json())
@@ -31,7 +18,8 @@ document.addEventListener("DOMContentLoaded", (event) => {
     function renderGame(game) {
         console.log(game)
         buildGrid()
-
+        //have to change this to find a game from an array of games, eventually
+        grid.dataset.id = game[0].id
         pizza.dataset.value = game[0].frozen_pizza
         soap.dataset.value = game[0].hand_soap
         toilet.dataset.value = game[0].toilet_paper
@@ -43,6 +31,17 @@ document.addEventListener("DOMContentLoaded", (event) => {
         ani_c.innerText = `Animal Crossing value: ${game[0].animal_crossing}`
     }
 
+    function buildGrid() {
+        let counter = 0
+        let grid = document.querySelector('.grid-container')
+        while (counter < 40) {
+            let div = document.createElement('div')
+            div.setAttribute("class", "grid-item")
+            grid.appendChild(div)
+            counter++
+        }
+    }
+
     die.addEventListener("click", (event) => {
         interpretDice()
     })
@@ -52,40 +51,66 @@ document.addEventListener("DOMContentLoaded", (event) => {
         let results = rollDice()
 
         //using the results hash, change the values of each commodity 
+        //lots of repeated code, can be put into a function
         switch (results.comodity) {
             case 1:
-                console.log("Toilet Paper")
                 if (results.direction === 1) {
-                    toilet.dataset.value++
+                    updateStock(1, "toilet_paper", results.number, grid.dataset.id)
                 } else {
-                    console.log(`down by ${results.number}`)
+                    updateStock(-1, "toilet_paper", results.number, grid.dataset.id)
                 }
                 break;
             case 2:
-                console.log("Animal Crossing")
                 if (results.direction === 1) {
-                    console.log(`up by ${results.number}`)
+                    updateStock(1, "animal_crossing", results.number, grid.dataset.id)
                 } else {
-                    console.log(`down by ${results.number}`)
+                    updateStock(-1, "animal_crossing", results.number, grid.dataset.id)
                 }
                 break;
             case 3:
-                console.log("Frozen Pizza")
                 if (results.direction === 1) {
-                    console.log(`up by ${results.number}`)
+                    updateStock(1, "frozen_pizza", results.number, grid.dataset.id)
                 } else {
-                    console.log(`down by ${results.number}`)
+                    updateStock(-1, "frozen_pizza", results.number, grid.dataset.id)
                 }
                 break;
             case 4:
-                console.log("Hand Soap")
                 if (results.direction === 1) {
-                    console.log(`up by ${results.number}`)
+                    updateStock(1, "hand_soap", results.number, grid.dataset.id)
                 } else {
-                    console.log(`down by ${results.number}`)
+                    updateStock(-1, "hand_soap", results.number, grid.dataset.id)
                 }
                 break;
         }
+    }
+
+    function updateStock(direction, comodity, change, game_id) {
+        let stock = document.getElementById(comodity)
+        let currentValue = parseInt(stock.dataset.value)
+        let newValue = parseInt(currentValue) + (change * 25 * direction)
+        let config = {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json'
+        }
+
+        if (newValue > 500) {
+            console.log(`Max value for ${comodity}`)
+            newValue = 250
+        } else if (newValue < 0) {
+            console.log(`Min value for ${comodity}`)
+            newValue = 250
+        }
+        stock.dataset.value = newValue
+        stock.innerText = `${comodity} value: ${newValue}`
+
+        fetch(`http://localhost:3000/api/games/${game_id}`, {
+            method: "POST",
+            headers: config,
+            body: JSON.stringify({ comodity: newValue })
+        })
+            .then(resp => resp.json())
+            .then(data => console.log(data))
+
     }
 
     //helper function that returns an object
